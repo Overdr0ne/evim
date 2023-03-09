80fd077729ff276099611a005da207eba4710e06
Overdr0ne <scmorris.dev@gmail.com>
2023-03-08 21:34:02 -0800

    emotion squash

--- emotion.el
+++ emotion.el
@@ -1,3 +1,28 @@
+;;; emotion.el --- emacs motion commands             -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Sam
+
+;; Author: Sam <scmorris.dev@gmail.com>
+;; Keywords: lisp, tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
 
 (defun emotion-cmd (cmd start-motion end-motion)
   (let ((beg
@@ -17,3 +42,4 @@
        (emotion-cmd ,cmd ,start-motion ,end-motion))))
 
 (provide 'emotion)
+;;; emotion.el ends here
