5a98ee3f1edd77f66a1e0d7fdc4e9e8b48df42ff
Overdr0ne <scmorris.dev@gmail.com>
2023-03-08 21:34:02 -0800

    stash

--- evim.el
+++ evim.el
@@ -24,9 +24,13 @@
 
 ;;; Code:
 
+(add-to-list 'load-path (file-name-directory (f-this-file)))
+
+(require 'untermin)
 (require 'dash)
 (require 'cl-macs)
 (require 'cl-extra)
+(require 'state)
 
 (defun evim-motion-cmd (cmd start-motion end-motion)
   (let ((beg (save-excursion
@@ -97,9 +101,7 @@
   (kill-region start end))
 
 (evim-define-interface evim--delete "delete" "d")
-;; (general-define-key :keymaps root-modes :states '(visual) "d" #'kill-region)
 (evim-define-normal-cmd evim-D "Emulate VIM D command." #'evim--delete nil #'end-of-line)
-;; (general-define-key :keymaps root-modes :states '(normal visual) "D" #'evim-D)
 (defun evim-visual-delete ()
   "Delete region."
   (interactive)
@@ -114,10 +116,7 @@
   (kill-ring-save start end))
 
 (evim-define-interface evim--yank "yank" "y")
-;; (general-define-key :keymaps root-modes :states '(visual) "y" #'copy-region-as-kill)
 (evim-define-normal-cmd evim-Y "Emulate VIM Y command." #'evim--yank nil #'end-of-line)
-;; (general-define-key :keymaps root-modes :states '(normal visual) "Y" #'evim-Y)
-;; (general-define-key :keymaps root-modes :states '(visual) "y" #'copy-region-as-kill)
 
 (defun evim--cut (start end)
   "Cut text from START to END position."
@@ -126,7 +125,6 @@
 
 (evim-define-interface evim--cut "cut" "c")
 (evim-define-normal-cmd evim-C "Emulate VIM C command." #'evim--cut nil #'end-of-line)
-;; (general-define-key :keymaps root-modes :states '(normal visual) "C" #'evim-C)
 
 (defun evim-paste-at (pos)
   (save-excursion
@@ -141,7 +139,6 @@
 ;; (general-define-key :keymaps root-modes :states '(normal visual) "p" nil)
 (evim-define-interface evim--paste "paste" "p")
 (evim-define-normal-cmd evim-P "Emulate VIM P command." #'evim--paste nil #'beginning-of-line)
-;; (general-define-key :keymaps root-modes :states '(normal visual) "P" #'evim-P)
 
 (defun evim-pp ()
   (interactive)
@@ -163,23 +160,16 @@
   (interactive)
   (evim-paste-at (1+ (line-end-position))))
 
-;; (general-define-key
-;;  :states '(normal visual)
-;;  :keymaps root-modes
-;;  "p" '(:ignore t :which-key "paste")
-;;  "pj"  #'evim-pj
-;;  "pk"  #'evim-pk)
-
 (defvar evim-insert-keymap (make-composed-keymap '(text-mode-map)))
 (defvar evim-normal-keymap (make-sparse-keymap))
 (defvar evim-visual-keymap (make-sparse-keymap))
 
-(defvar evim-cur-mode 'evim-normal-mode)
-
 (defmacro evim-define-mode (name)
-  `(progn
+  `(let ((mode-sym ',(intern (concat "evim-" (symbol-name name) "-mode"))))
      (defun ,(intern (concat "evim--" (symbol-name name) "-mode-enable")) nil)
      (defun ,(intern (concat "evim--" (symbol-name name) "-mode-disable")) nil)
+     (defun ,(intern (concat "evim-" (symbol-name name) "-mode-enable")) () (interactive) (funcall mode-sym +1))
+     (defun ,(intern (concat "evim-" (symbol-name name) "-mode-disable")) () (interactive) (funcall mode-sym -1))
      (define-minor-mode ,(intern (concat "evim-" (symbol-name name) "-mode"))
        ,(concat "Toggle Evim " (symbol-name name) " minor mode.
 
@@ -194,29 +184,19 @@ the mode, `toggle' toggles the state.")
        (let ((mode-sym ',(intern (concat "evim-" (symbol-name name) "-mode")))
              (enable-sym ',(intern (concat "evim--" (symbol-name name) "-mode-enable")))
              (disable-sym ',(intern (concat "evim--" (symbol-name name) "-mode-disable"))))
-         (when (not (equal evim-cur-mode mode-sym))
-           (message "here")
-           (funcall evim-cur-mode -1)
-           (set evim-cur-mode mode-sym)
-           )
          (if (symbol-value mode-sym)
              (funcall enable-sym)
            (funcall disable-sym))))))
 
-(define-key input-decode-map
-    (kbd "C-[")
-    [control-leftbracket])
 (defun evim-define-keys (keymaps defs)
   (dolist (def defs)
     (let* ((cmd-keys (nth 0 def))
            (cmd-sym (nth 1 def))
            (cmd-keys (if (stringp cmd-keys)
-                        (kbd cmd-keys)
-                      cmd-keys)))
+                         (kbd cmd-keys)
+                       cmd-keys)))
       (dolist (keymap keymaps)
-            (define-key (symbol-value keymap) cmd-keys cmd-sym)
-          ))))
-
+        (define-key (symbol-value keymap) cmd-keys cmd-sym)))))
 
 (evim-define-mode insert)
 (evim-define-keys
@@ -226,48 +206,207 @@ the mode, `toggle' toggles the state.")
    ("C-e" end-of-line)
    ("C-f" forward-char)
    ("C-b" backward-char)
+   ("C-l" completion-at-point)
    ("C-n" next-line)
    ("C-p" previous-line)
    ("C-s" isearch-forward)
    ("C-v" yank)
-   ("M-s" isearch-repeat-forward)
    ("C-h" xah-delete-backward-char-or-bracket-text)
-   ("M-h" xah-delete-backward-bracket-text)
-   ("M-w" forward-word)
-   ("M-b" backward-word)
+   ;; ("M-w" forward-word)
+   ;; ("M-b" backward-word)
    ("<return>" newline-and-indent)
-   ([control-leftbracket] evim-normal-mode)
+   ("<C-[>" (lambda () (interactive) (state-transition 'evim-insert-mode 'evim-normal-mode))))
+ )
+
+(defun evim-open-line-below ()
+  (interactive)
+  (end-of-line)
+  (open-line 1)
+  (next-line)
+  (state-transition 'evim-normal-mode 'evim-insert-mode))
+
+(defun evim-open-line-above ()
+  (interactive)
+  (beginning-of-line)
+  (open-line 1)
+  (state-transition 'evim-normal-mode 'evim-insert-mode))
+
+(defun evim-join ()
+  (interactive)
+  (save-excursion
+    (end-of-line)
+    (forward-char)
+    (join-line)))
+
+(defun evim-A ()
+  (interactive)
+  (end-of-line)
+  (state-transition 'evim-normal-mode 'evim-insert-mode))
+
+(defun evim-a ()
+  (interactive)
+  (forward-char)
+  (state-transition 'evim-normal-mode 'evim-insert-mode))
+
+(defun evim-H ()
+  (interactive)
+  (move-to-window-line 0))
+
+(defun evim-i ()
+  (interactive)
+  (state-transition 'evim-normal-mode 'evim-insert-mode))
+
+(defun evim-L ()
+  (interactive)
+  (move-to-window-line -1))
+
+(defun evim-v ()
+  (interactive)
+  (state-transition 'evim-normal-mode 'evim-visual-mode))
+
+(defun evim-V ()
+  (interactive)
+  (save-excursion
+    (next-line)
+    (beginning-of-line)
+    (state-transition 'evim-normal-mode 'evim-visual-mode)))
+
+(defun evim-x ()
+  (interactive)
+  (delete-char 1))
+
+(defvar evim-lb-keymap (make-sparse-keymap))
+(evim-define-keys
+ '(evim-lb-keymap)
+ `(
+   ("(" backward-up-list)
+   ("[" beginning-of-defun)
    ))
+(defvar evim-rb-keymap (make-sparse-keymap))
+(evim-define-keys
+ '(evim-rb-keymap)
+ `(
+   (")" up-list)
+   ("]" end-of-defun)))
+
+(defvar evim-g-keymap (make-sparse-keymap))
+(evim-define-keys
+ '(evim-g-keymap)
+ `(
+   ("g" beginning-of-buffer)
+   ("G" end-of-buffer)))
+
+(defvar evim-z-keymap (make-sparse-keymap))
+(evim-define-keys
+ '(evim-z-keymap)
+ `(
+   ("z" recenter)))
+
+(defun evim-replace-char (newc)
+  (interactive "sEnter char: ")
+  (save-excursion
+    (let* ((beg (point))
+           (end (1+ (point)))
+           (oldc (buffer-substring-no-properties beg end)))
+      (replace-string-in-region oldc newc beg end))))
 
 (evim-define-mode normal)
-(evim-define-keys
- '(evim-normal-keymap)
- `(
-   ("h" backward-char)
-   ("j" next-line)
-   ("k" previous-line)
-   ("l" forward-char)
-   ("i" evim-insert-mode)
-   ("p" ,evim-paste-keymap)
-   ("y" ,evim-yank-keymap)
-   ("c" ,evim-cut-keymap)
-   ("u" undo)
- ))
 
 (define-key evim-normal-keymap [control-bracketleft] #'evim-normal-mode)
 (defun evim--normal-mode-enable () (setq-local cursor-type t))
 (defun evim--insert-mode-enable () (setq-local cursor-type 'bar))
 (defun evim--insert-mode-disable () (setq-local cursor-type t))
+
+(evim-define-keys
+ '(evim-normal-keymap evim-visual-keymap)
+ `(
+   ("[" ,evim-lb-keymap)
+   ("]" ,evim-rb-keymap)
+   ("= =" indent-relative)
+   ("= a" sam-indent-all)
+   ("0" beginning-of-line)
+   ("$" end-of-line)
+   ("a" evim-a)
+   ("A" evim-A)
+   ("b" backward-word)
+   ("c" ,evim-cut-keymap)
+   ("d" ,evim-delete-keymap)
+   ("e" forward-word)
+   ("g" ,evim-g-keymap)
+   ("G" end-of-buffer)
+   ("h" backward-char)
+   ("H" evim-H)
+   ("i" evim-i)
+   ("j" next-line)
+   ("k" previous-line)
+   ("l" forward-char)
+   ("L" evim-L)
+   ("M" move-to-window-line-top-bottom)
+   ("n" isearch-repeat-forwar)
+   ("o" evim-open-line-below)
+   ("O" evim-open-line-above)
+   ("p" ,evim-paste-keymap)
+   ("r" evim-replace-char)
+   ("u" undo)
+   ("v" evim-v)
+   ("V" evim-V)
+   ("y" ,evim-yank-keymap)
+   ("w" forward-to-word)
+   ("c" ,evim-cut-keymap)
+   ("x" evim-x)
+   ("u" undo)
+   ("z" ,evim-z-keymap)
+   ))
+
+(defface evim-highlight
+  '((t
+     :inherit region
+     ;; :box (:line-width -3 :style released-button)
+     ;; :weight bold
+     ))
+  "Face for evim highlighting.")
+
 (evim-define-mode visual)
+(defvar evim-mark (make-marker))
+(defvar evim-region-overlay nil)
+(defun evim-set-marker ()
+  (interactive)
+  (set-marker evim-mark (point)))
+(defun evim-highlight-region ()
+  (interactive)
+  (if evim-region-overlay
+      (move-overlay evim-region-overlay (marker-position evim-mark) (point))
+    (setq evim-region-overlay (make-overlay (marker-position evim-mark) (point))))
+  (overlay-put evim-region-overlay 'face 'evim-highlight))
+(defun evim-unhighlight-region ()
+  (interactive)
+  (delete-overlay evim-region-overlay))
+(defun evim--visual-mode-enable ()
+  (interactive)
+  (evim-set-marker)
+  (add-hook 'post-command-hook 'evim-highlight-region))
+(defun evim--visual-mode-disable ()
+  (interactive)
+  (remove-hook 'post-command-hook 'evim-highlight-region)
+  (evim-unhighlight-region))
+(defun evim-kill-region ()
+  (interactive)
+  (kill-region (marker-position evim-mark) (point))
+  (state-transition 'evim-visual-mode 'evim-normal-mode))
+(defun evim-copy-region-as-kill ()
+  (interactive)
+  (copy-region-as-kill (marker-position evim-mark) (point))
+  (state-transition 'evim-visual-mode 'evim-normal-mode))
+(defun evim-visual-escape ()
+  (interactive)
+  (state-transition 'evim-visual-mode 'evim-normal-mode))
 (evim-define-keys
  '(evim-visual-keymap)
  '(
-   ("d" #'kill-region)
-   ("y" #'copy-region-as-kill)
-   )
- )
-(defun evim--visual-mode-enable () (set-mark-command nil))
-(defun evim--visual-mode-disable () (keyboard-quit))
+   ("<C-[>" evim-visual-escape)
+   ("C-g" evim-visual-escape)
+   ("d" evim-kill-region)
+   ("y" evim-copy-region-as-kill)))
 
 (provide 'evim)
 ;;; evim.el ends here
